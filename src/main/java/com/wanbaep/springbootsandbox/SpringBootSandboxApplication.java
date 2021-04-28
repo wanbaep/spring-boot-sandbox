f2836507425a1d63337f88bcd484c9caad0e0a6c
wanbaep <wanbaepp@naver.com>
2021-04-29 08:16:13 +0900

    Spring boot 프로젝트 생성

---
+++ src/main/java/com/wanbaep/springbootsandbox/SpringBootSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.wanbaep.springbootsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSandboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wanbaep/springbootsandbox/SpringBootSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanbaep.springbootsandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
